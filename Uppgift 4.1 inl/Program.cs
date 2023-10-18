f1a38c97bccc0117da80b78ba6666f467b1568ec
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-10-18 10:14:17 +0200

    Add project files.

---
+++ Uppgift 4.1 inl/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 30; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int a = 200; a >= 180; a--)
+            {
+                Console.WriteLine(a);
+            }
+
+            for (int b = 1000; b <= 1400; b=b+50)
+            {
+                Console.WriteLine(b);
+            }
+
+        }
+    }
+
+
+}
